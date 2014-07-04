84911e7c308e38aa1f5c4f36ff0efea1f6ac29fe
Cory Nelson <corynelson@hotmail.co.uk>
2014-07-04 14:09:45 +0100

    Initial Commit

--- src/com/rs2/server/Server.java
+++ src/com/rs2/server/Server.java
@@ -5,15 +5,21 @@ package com.rs2.server;
  *     on 04/07/14.
  *     at 14:06.
  */
-public class Server {
+public class Server implements Runnable {
 
     private static Server ourInstance;
 
     public static void main(String[] args) {
         Server.ourInstance = new Server();
+        new Thread(Server.ourInstance).start();
     }
 
     private Server() {
+
+    }
+
+    @Override
+    public void run() {
 
     }
 
